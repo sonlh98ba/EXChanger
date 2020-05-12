b61e35c1885813685e550db49c0791e4e3589bbb
Son Le Hong <41584436+sonlh98ba@users.noreply.github.com>
2020-05-12 17:10:35 +0700

    add

---
+++ EXChanger/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace EXChanger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.lstCustomer = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExit.Location = new System.Drawing.Point(617, 296);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "Thoát";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnExport.Location = new System.Drawing.Point(536, 296);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(75, 23);
+            this.btnExport.TabIndex = 1;
+            this.btnExport.Text = "Xuất";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // btnImport
+            // 
+            this.btnImport.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnImport.Location = new System.Drawing.Point(12, 296);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(75, 23);
+            this.btnImport.TabIndex = 2;
+            this.btnImport.Text = "Nhập";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // lblPath
+            // 
+            this.lblPath.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.lblPath.Location = new System.Drawing.Point(93, 298);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(391, 18);
+            this.lblPath.TabIndex = 3;
+            this.lblPath.Text = "Path:";
+            this.lblPath.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lstCustomer
+            // 
+            this.lstCustomer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstCustomer.FullRowSelect = true;
+            this.lstCustomer.GridLines = true;
+            this.lstCustomer.Location = new System.Drawing.Point(12, 12);
+            this.lstCustomer.Name = "lstCustomer";
+            this.lstCustomer.Size = new System.Drawing.Size(680, 277);
+            this.lstCustomer.TabIndex = 4;
+            this.lstCustomer.UseCompatibleStateImageBehavior = false;
+            this.lstCustomer.View = System.Windows.Forms.View.Details;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 330);
+            this.Controls.Add(this.lstCustomer);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.btnExit);
+            this.Name = "Form1";
+            this.Text = "EXChanger - by Son Le Hong";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.ListView lstCustomer;
+    }
+}
+
---
+++ EXChanger/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace EXChanger
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            // Tạo file mở
+            OpenFileDialog fOpen = new OpenFileDialog();
+            fOpen.Filter = "(Tất cả các tệp)|*.*|(Các tệp excel)|*.xlsx";
+            fOpen.ShowDialog();
+
+            // Xử lý
+            if (fOpen.FileName!="")
+            {
+                lblPath.Text = fOpen.FileName;
+
+                // Tạo đối tượng
+
+                Excel.Application app = new Excel.Application();
+
+                // Mở tệp
+
+                Excel.Workbook wb = app.Workbooks.Open(fOpen.FileName);
+
+                try
+                {
+                    // Mở sheet
+                    Excel._Worksheet sheet = wb.Sheets[1];
+                    Excel.Range range = sheet.UsedRange;
+                    // Đọc dữ liệu
+
+                    int rows = range.Rows.Count;
+                    int columns = range.Columns.Count;
+
+                    // Bắt đầu listview
+
+                    // Tiêu đề
+                    for (int i=1;i<=columns;i++)
+                    {
+                        string columnName = range.Cells[1, i].Value.ToString();
+                        ColumnHeader col = new ColumnHeader();
+                        col.Text = columnName;
+                        lstCustomer.Columns.Add(col);
+                    }
+
+                    // Dữ liệu
+
+
+                    // Nhập
+                    for (int i = 2;i <= rows;i++)
+                    {
+                        ListViewItem item = new ListViewItem();
+                        for (int j = 1;j <= columns;j++)
+                        {
+                            if (j == 1)
+                            {
+                                if (range.Cells[i, j].Value == null)
+                                    item.Text = "";
+                                else
+                                    item.Text = range.Cells[i, j].Value.ToString();
+                            }
+                            else
+                            {
+                                if (range.Cells[i, j].Value == null)
+                                    item.SubItems.Add("");
+                                else
+                                    item.SubItems.Add(range.Cells[i, j].Value.ToString());
+                            }
+                        }
+
+                        lstCustomer.Items.Add(item);
+                    }
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Chọn lại tập tin!");
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            // Tạo file lưu
+            SaveFileDialog fSave = new SaveFileDialog();
+            fSave.Filter = "(Tất cả các tệp)|*.*|(Các tệp excel)|*.xlsx";
+            fSave.ShowDialog();
+
+            // Xử lý
+            if (fSave.FileName != "")
+            {
+                Excel.Application app = new Excel.Application();
+
+                Excel.Workbook wb = app.Workbooks.Add(Type.Missing);
+
+                Excel._Worksheet sheet = null;
+
+                try 
+                { 
+                    // Đọc dữ liệu từ listview
+
+                    sheet = wb.ActiveSheet;
+                    sheet.Name = "Dữ liệu xuất";
+
+                    for (int i=1;i<10;i++)
+                    {
+
+                    }
+                
+                }
+                catch (Exception ex)
+                { 
+                   MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    app.Quit();
+                    wb = null;
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Chọn lại tập tin!");
+            }
+        }
+    }
+}
